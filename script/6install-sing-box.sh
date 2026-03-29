ecbc0dbb7c4edc0ab1204cb0b86e6bd463714d76
you10069 <104882329+you10069@users.noreply.github.com>
2026-03-28 21:29:14 -0700

    Update 6install-sing-box.sh

--- script/6install-sing-box.sh
+++ script/6install-sing-box.sh
@@ -121,7 +121,7 @@ echo -e "${YELLOW}✔ Sing-Box 下载完成${RESET}"
 # ---------------------------------------------------------
 echo -e "${BLUE}[6/8] 创建目录结构与配置文件 ...${RESET}"
 
-mkdir -p /etc/sing-box
+mkdir -p /etc/sing-box/iptables
 mkdir -p /etc/sing-box/crt
 
 touch /etc/sing-box/config_template.json
