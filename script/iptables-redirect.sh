eee8f68b418d5e9aced61004e305a2155f2c5b74
you10069 <104882329+you10069@users.noreply.github.com>
2026-03-28 21:23:24 -0700

    Update iptables-redirect.sh

--- script/iptables-redirect.sh
+++ script/iptables-redirect.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 iptables -t nat -A PREROUTING -i eth0 -p udp --dport 10000:20000 -j REDIRECT --to-ports 8443
 ip6tables -t nat -A PREROUTING -i eth0 -p udp --dport 10000:20000 -j REDIRECT --to-ports 8443
-iptables -t nat -A PREROUTING -i eth0 -p udp --dport 21000:29000 -j REDIRECT --to-ports 55555
-ip6tables -t nat -A PREROUTING -i eth0 -p udp --dport 21000:29000 -j REDIRECT --to-ports 55555
+iptables -t nat -A PREROUTING -i eth0 -p udp --dport 21000:29000 -j REDIRECT --to-ports 8444
+ip6tables -t nat -A PREROUTING -i eth0 -p udp --dport 21000:29000 -j REDIRECT --to-ports 8444
+iptables -t nat -A PREROUTING -i eth0 -p udp --dport 21000:29000 -j REDIRECT --to-ports 8445
+ip6tables -t nat -A PREROUTING -i eth0 -p udp --dport 21000:29000 -j REDIRECT --to-ports 8445
