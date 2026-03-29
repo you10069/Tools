3d856721b747d14c7f16d877be7893c74fe03bae
you10069 <104882329+you10069@users.noreply.github.com>
2026-03-28 21:19:40 -0700

    Create iptables-redirect.sh

---
+++ script/iptables-redirect.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+iptables -t nat -A PREROUTING -i eth0 -p udp --dport 10000:20000 -j REDIRECT --to-ports 8443
+ip6tables -t nat -A PREROUTING -i eth0 -p udp --dport 10000:20000 -j REDIRECT --to-ports 8443
+iptables -t nat -A PREROUTING -i eth0 -p udp --dport 21000:29000 -j REDIRECT --to-ports 55555
+ip6tables -t nat -A PREROUTING -i eth0 -p udp --dport 21000:29000 -j REDIRECT --to-ports 55555
